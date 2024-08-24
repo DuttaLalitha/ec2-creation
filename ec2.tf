23246968ec8164875315f588f254b0447d078040
DuttaLalitha <duttalalitha123@gmail.com>
2024-08-24 19:25:09 +0530

    ec2 creation

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0a0e5d9c7acc336f1"
+  key_name = "keypairfirst"
+  instance_type = "t2.micro"
+  security_groups = [aws_default_security_group.default.id]
+  subnet_id = aws_subnet.main.id
+  user_data = file("applications.sh")
+
+  tags = {
+    Name = "aws_vpc_instance"
+  }
+}
