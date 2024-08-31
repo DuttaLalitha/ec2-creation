1a6fd7ba552185f5e4f618541d2db1d61dd6f94b
DuttaLalitha <duttalalitha123@gmail.com>
2024-08-31 14:05:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0a0e5d9c7acc336f1"
-  key_name = "keypairfirst"
+  key_name = "mummylap"
   instance_type = "t2.micro"
   security_groups = [aws_default_security_group.default.id]
   subnet_id = aws_subnet.main.id
